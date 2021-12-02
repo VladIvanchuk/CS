ebc6cf8dd88bdb12422fe679feba0f4a29e2b15f
VladIvanchuk <73890367+VladIvanchuk@users.noreply.github.com>
2021-12-02 21:18:17 +0200

    Add files via upload

---
+++ Radius/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int CountSide;
+            double side;
+            double r;
+            Console.WriteLine("input radius : ");
+            r = Convert.ToInt32(Console.ReadLine());
+            while (r <= 0)
+            {
+                Console.WriteLine("input radius more zero : ");
+                r = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("input Count side : ");
+            CountSide = Convert.ToInt32(Console.ReadLine());
+            while (CountSide <= 0)
+            {
+                Console.WriteLine("input Count side more zero : ");
+                CountSide = Convert.ToInt32(Console.ReadLine());
+            }
+            side = 2 * r * Math.Tan(Math.PI / CountSide);
+            Console.WriteLine(side);
+
+            Console.WriteLine(side * CountSide);
+        }
+    }
+}
