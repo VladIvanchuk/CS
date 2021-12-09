5caaa625262d62e1a25d65af2cdd9185fff55a5e
VladIvanchuk <73890367+VladIvanchuk@users.noreply.github.com>
2021-12-09 08:23:21 +0200

    Add files via upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+
+    //8.	Перевірити істинність вислову: "Серед трьох даних цілих чисел є хоч би одна пара взаємно протилежних". 
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number: ");
+            int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNum == secondNum * (-1) || secondNum == thirdNum * (-1) || firstNum == thirdNum * (-1))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }  
+        }
+    }
+}
